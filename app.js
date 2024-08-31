d5efa29defa88e8db87d06ffb7fe527275b84836
myharton <168491250+myharton@users.noreply.github.com>
2024-08-31 17:11:47 -0400

    react installed, app.js file added, google calendar

---
+++ app.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg'
+import '/app.css'
+import {GoogleLogin} from  'react-google-login'
+
+function App() {
+
+    return(
+        <div>
+            <div classname = 'App'>
+                <H1>The CPL Room Scheduler</H1>
+        </div>
+        <div>
+
+<GoogleLogin clientId = ></GoogleLogin> 
+ 
+        </div>
+        </div>
+
+
+
+    )
+}
+
+
+/// create google calendar then place clientId into <GoogleLogin> tag
